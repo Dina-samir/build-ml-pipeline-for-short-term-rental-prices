4ab3f05e52bdd83a530eeefdf0e66fcd9174f0f9
Dina-samir <36201487+Dina-samir@users.noreply.github.com>
2023-12-30 19:44:21 +0200

    Update run.py

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,7 +36,9 @@ def go(args):
     min_price = args.min_price
     max_price = args.max_price
     idx = df['price'].between(min_price, max_price)
- 
+    df = df[idx].copy()
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     
     # Convert last_review to datetime
