851a6c5cc8fff61a3cfdee1d7f7f80b0efd61bbe
Dina-samir <36201487+Dina-samir@users.noreply.github.com>
2023-12-30 19:34:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def go(config: DictConfig):
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
+                json.dump(dict(config["modeling"]["random_forest_pipeline"]["random_forest"].items()), fp)
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
